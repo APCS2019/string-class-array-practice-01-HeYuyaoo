23c2fb4a7dea81d3c2ee4133297d7e259518ac46
HeYuyaoo <45375765+HeYuyaoo@users.noreply.github.com>
2018-12-04 09:08:20 +0800

    Create 20181204-Caroline.java

---
+++ 20181204-Caroline.java
@@ -0,0 +1,22 @@
+Past paper 2015
+main idea: String, class
+public class HiddenWord{
+    private String realWord;
+    public HiddenWord(String s1){
+        realWord=s1;
+    }
+    public String getHint(String guess){
+    String temp="";
+    for(int i=0;i<guess.length();i++){
+        if(realWord.subString(i,i+1).equals(guess.subString(i,i+1)){
+            temp=temp+realWord.subString(i,i+1);
+        }
+        else if(realWord.indexOf(guess.subString(i,i+1)!=-1){
+            temp=temp+"+";
+        }
+         else{
+         temp=temp+"*";
+        }
+     }
+     return temp;
+     }
